1957127cbe73f947be2a02d45fc60ad32eb8bb4a
dimash <dinmukhamed.t@tinkerteam.ai>
2023-11-08 21:15:53 +0600

    added authentication and active user requirements for routes

--- greenlight/cmd/api/routes.go
+++ greenlight/cmd/api/routes.go
@@ -17,11 +17,11 @@ func (app *application) routes() http.Handler {
 	// endpoints using the HandlerFunc() method
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/crafting_materials", app.listCraftingMaterialsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/crafting_materials", app.createCraftingMaterialHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/crafting_materials/:id", app.showCraftingMaterialHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/crafting_materials/:id", app.updateCraftingMaterialHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/crafting_materials/:id", app.deleteCraftingMaterialHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/crafting_materials", app.requireActivatedUser(app.listCraftingMaterialsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/crafting_materials", app.requireActivatedUser(app.createCraftingMaterialHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/crafting_materials/:id", app.requireActivatedUser(app.showCraftingMaterialHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/crafting_materials/:id", app.requireActivatedUser(app.updateCraftingMaterialHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/crafting_materials/:id", app.requireActivatedUser(app.deleteCraftingMaterialHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
