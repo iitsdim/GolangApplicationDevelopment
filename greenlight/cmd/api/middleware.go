ea0e94a1dd5ffb3b5cb439497f21dfd5ee3f4b38
dimash <dinmukhamed.t@tinkerteam.ai>
2023-11-10 11:34:21 +0600

    add unlimited CORS

--- greenlight/cmd/api/middleware.go
+++ greenlight/cmd/api/middleware.go
@@ -158,3 +158,11 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
